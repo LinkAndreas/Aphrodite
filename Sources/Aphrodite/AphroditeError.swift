15952ab35eb69cdf1a814785a28ba006332e664e
Andreas Link <mail@linkandreas.de>
2020-06-11 13:32:35 +0200

    Adapt error handling

--- Sources/Aphrodite/AphroditeError.swift
+++ Sources/Aphrodite/AphroditeError.swift
@@ -3,36 +3,17 @@
 import Foundation
 
 public enum AphroditeError: Error {
-    case unauthorized(HTTPURLResponse)
-    case notFound(HTTPURLResponse)
-    case forbidden(HTTPURLResponse)
-    case underlying(HTTPURLResponse, Error)
-    case client(HTTPURLResponse, StatusCode)
-    case server(HTTPURLResponse, StatusCode)
-    case decoding(DecodingError)
+    case unauthorized(HTTPURLResponse, Data)
+    case notFound(HTTPURLResponse, Data)
+    case forbidden(HTTPURLResponse, Data)
+    case underlying(HTTPURLResponse, Data, Error)
+    case client(HTTPURLResponse, Data, StatusCode)
+    case server(HTTPURLResponse, Data, StatusCode)
+    case decoding(HTTPURLResponse, Data, DecodingError)
     case encoding(EncodingError)
     case serviceCancelled
     case notConnectedToInternet
     case unexpected
-
-    var httpUrlRepsonse: HTTPURLResponse? {
-        switch self {
-        case let .unauthorized(response):
-            return response
-
-        case let .notFound(response):
-            return response
-
-        case let .forbidden(response):
-            return response
-
-        case let .underlying(response, _):
-            return response
-
-        default:
-            return nil
-        }
-    }
 }
 
 extension AphroditeError: CustomStringConvertible {
