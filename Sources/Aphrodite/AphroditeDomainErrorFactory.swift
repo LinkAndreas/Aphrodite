81ec879fa0bea191fac3bc16b016fec7b6701a2d
Andreas Link <mail@linkandreas.de>
2020-09-27 08:29:57 +0200

    Document AphroditeError as well as the associated factories

--- Sources/Aphrodite/AphroditeDomainErrorFactory.swift
+++ Sources/Aphrodite/AphroditeDomainErrorFactory.swift
@@ -1,10 +1,10 @@
 //  Copyright © 2020 Andreas Link. All rights reserved.
 
-/// A blueprint including the requirements an AphroditeDomainErrorFactory must fulfill
+/// A blueprint including the requirements an `AphroditeDomainErrorFactory` must fulfill
 public protocol AphroditeDomainErrorFactory {
-    /// The domain error type an AphroditeError is mapped to
+    /// The domain error type an `AphroditeError` is mapped to
     associatedtype DomainError: Error
 
-    /// The function responsible for transforming an AphroditeError into the associated DomainError
+    /// The function responsible for transforming an `AphroditeError` into the associated DomainError
     static func make(from error: AphroditeError) -> DomainError
 }
