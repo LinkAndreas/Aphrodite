59e8581248caa054bce6069bcb9ee02a40aceeb8
Andreas Link <mail@linkandreas.de>
2020-10-03 13:49:13 +0200

    Document ParameterEncodings

--- Sources/Aphrodite/Globals/Extensions/CharacterSetExtension.swift
+++ Sources/Aphrodite/Globals/Extensions/CharacterSetExtension.swift
@@ -6,7 +6,7 @@ internal extension CharacterSet {
     /**
      The `CharacterSet` only including characters that are allowed in RFC 3986.
 
-     Adapted from: https://github.com/Alamofire/Alamofire/blob/master/Source/URLEncodedFormEncoder.swift
+     - Note: Adapted from: https://github.com/Alamofire/Alamofire/blob/master/Source/URLEncodedFormEncoder.swift
      */
     static let urlQueryAllowedRfc3986: CharacterSet = {
         let generalDelimitersToEncode = ":#[]@"
